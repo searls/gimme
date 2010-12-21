f18e6c51273f730a25dcd1c12863b232e95cdeae
Justin Searls <searls@gmail.com>
2010-12-21 10:40:26 -0500

    Refactored the steps nicely; created a silly animal/dog/cat class hierarchy to work against as I prove out stubbing more and later introduce matchers

---
+++ features/support/dog.rb
@@ -0,0 +1,38 @@
+
+# A simple class hierarchy declaring a few different types of methods on it that I can write cucumber features against something other than standard ruby classes/methods
+
+class Animal
+end
+
+class Dog < Animal
+  # to exercise stub & verify of attributes: matching(regex)
+  attr_accessor :name
+  
+  def walk_to(x,y)
+    'use me to exercise numeric matchers: numeric, less_than, greater_than, within_range'
+  end
+
+  def introduce_to(animal)
+    'use me to exercise identity matchers: anything, is_a(Animal), is_a(Cat), any(Animal), any(Dog)'
+  end
+  
+  def holler_at(loudly)
+    'use me to exercise: boolean'
+  end
+  
+  def use_toys(hash_of_toys_and_actions)
+    'use me to exercise: hash_including'
+  end
+  
+  def clean_toys(array_of_toys)
+    'use me to exercise: including'
+  end
+  
+  def purebred?
+    'stub me to return a boolean'
+  end
+  
+end
+
+class Cat < Animal
+end
