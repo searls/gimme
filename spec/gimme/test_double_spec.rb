17ee5ac9503be59575d34523cb15e55835f72c57
Justin Searls <searls@gmail.com>
2011-10-02 23:45:56 -0400

    started the test_double spec

---
+++ spec/gimme/test_double_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Gimme::TestDouble do
+  class MassiveDamage
+    def boom
+      :asplode
+    end
+  end
+
+  describe "#gimme_next" do
+    Given(:test_double) { gimme_next(MassiveDamage) }
+    Given(:subject) { MassiveDamage.new }
+
+    Given { give(test_double).boom { :kaboom } }
+    When(:result) { subject.boom }
+    Then { result.should == :kaboom }
+
+    context "subsequent uses" do
+      Given(:next_subject) { MassiveDamage.new }
+      When(:next_result) { next_subject.boom }
+      Then { next_result.should == :asplode }
+    end
+  end
+
+end
--- spec/gimme/verifies_spec.rb
+++ spec/gimme/verifies_spec.rb
@@ -5,12 +5,12 @@ describe Gimme::Verifies do
     def ferment(beans=nil,time=nil)
     end
   end
-  Given(:double) { gimme(Natto) }
+  Given(:test_double) { gimme(Natto) }
 
   shared_examples_for "a verifier" do
 
     context "invoked once when expected once" do
-      Given { double.ferment }
+      Given { test_double.ferment }
       When(:result) { lambda { verifier.ferment } }
       Then { result.should_not raise_error }
     end
@@ -21,22 +21,22 @@ describe Gimme::Verifies do
     end
 
     context "invoked with incorrect args" do
-      Given { double.ferment(5) }
+      Given { test_double.ferment(5) }
       When(:result) { lambda { verifier.ferment(4) } }
       Then { result.should raise_error Errors::VerificationFailedError }
     end
 
     context "invoked too few times" do
-      Given(:verifier) { Verifies.new(double,3) }
-      Given { 2.times { double.ferment } }
+      Given(:verifier) { Verifies.new(test_double,3) }
+      Given { 2.times { test_double.ferment } }
       When(:result) { lambda { verifier.ferment } }
       Then { result.should raise_error Errors::VerificationFailedError }
     end
 
     context "juggling multiple verifiers for the same method" do
-      Given(:multi_verifier) { Verifies.new(double,2) }
-      Given { double.ferment(:panda,:sauce) }
-      Given { 2.times { double.ferment(2,3) } }
+      Given(:multi_verifier) { Verifies.new(test_double,2) }
+      Given { test_double.ferment(:panda,:sauce) }
+      Given { 2.times { test_double.ferment(2,3) } }
       When(:result) do
         lambda do
           multi_verifier.ferment(2,3)
@@ -46,19 +46,19 @@ describe Gimme::Verifies do
       Then { result.should_not raise_error }
     end
 
-    context "a method not on the double" do
+    context "a method not on the test_double" do
       When(:result) { lambda { verifier.eat } }
       Then { result.should raise_error NoMethodError }
     end
 
     context "a satisfied argument matcher" do
-      Given { double.ferment(5) }
+      Given { test_double.ferment(5) }
       When(:result) { lambda { verifier.ferment(numeric) } }
       Then { result.should_not raise_error }
     end
 
     context "an unsatisifed argument matcher" do
-      Given { double.ferment("True") }
+      Given { test_double.ferment("True") }
       When(:result) { lambda { verifier.ferment(boolean) } }
       Then { result.should raise_error Errors::VerificationFailedError }
     end
@@ -66,19 +66,19 @@ describe Gimme::Verifies do
 
   shared_examples_for "an overridden verifier" do
     context "a method not on the double that is invoked" do
-      Given { double.eat }
+      Given { test_double.eat }
       When(:result) { lambda { verifier.eat } }
       Then { result.should_not raise_error }
     end
 
-    context "a method not on the double that is _not_ invoked" do
+    context "a method not on the test_double that is _not_ invoked" do
       When(:result) { lambda { verifier.eat } }
       Then { result.should raise_error Errors::VerificationFailedError }
     end
   end
 
   context "class API" do
-    Given(:verifier) { Verifies.new(double) }
+    Given(:verifier) { Verifies.new(test_double) }
     it_behaves_like "a verifier"
     it_behaves_like "an overridden verifier" do
       Given { verifier.raises_no_method_error = false }
@@ -87,11 +87,11 @@ describe Gimme::Verifies do
 
   context "gimme DSL" do
     it_behaves_like "a verifier" do
-      Given(:verifier) { verify(double) }
+      Given(:verifier) { verify(test_double) }
     end
 
     it_behaves_like "an overridden verifier" do
-      Given(:verifier) { verify!(double) }
+      Given(:verifier) { verify!(test_double) }
     end
   end
 
