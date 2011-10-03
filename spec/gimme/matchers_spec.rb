d7dc39f95851da91779fc39e9f5afc1fd4ab20e0
Justin Searls <searls@gmail.com>
2011-10-02 21:10:49 -0400

    added specs for the other types

--- spec/gimme/matchers_spec.rb
+++ spec/gimme/matchers_spec.rb
@@ -7,6 +7,12 @@ RSpec::Matchers.define :match do |expected|
 end
 
 describe Gimme::Matchers do
+  class Shoopuf
+  end
+
+  class BabyShoopuf < Shoopuf
+  end
+
   describe Matcher do
     context "a plain, default matcher" do
       Given(:matcher) { Matcher.new }
@@ -32,31 +38,88 @@ describe Gimme::Matchers do
     end
   end
 
-  describe IsA do
-
-    class Shoopuf
-    end
-
-    class BabyShoopuf < Shoopuf
-    end
-
-    shared_examples_for "an identity matcher" do
-      context "the argument is a matching type" do
-        Then { matcher.should match Shoopuf.new }
-      end
-
-      context "the argument is not a matching type" do
-        Then { matcher.should_not match "Some other class" }
-      end
-
-      context "the argument is a subclass of the type" do
-        Then { matcher.should match BabyShoopuf.new }
-      end
-    end
-
-    Given(:matcher) { IsA.new(Shoopuf) }
-
+  shared_examples_for "an identity matcher" do
+    Then { matcher.should match Shoopuf.new }
+    Then { matcher.should match BabyShoopuf.new }
   end
 
+  shared_examples_for "a restrictive matcher" do
+    Then { matcher.should_not match nil }
+    Then { matcher.should_not match "Pandas" }
+    Then { matcher.should_not match Object.new }
+    Then { matcher.should_not match Class }
+  end
+
+  shared_examples_for "a relaxed matcher" do
+    Then { matcher.should match nil }
+  end
+
+
+  describe IsA do
+    context "class API" do
+      Given(:matcher) { IsA.new(Shoopuf) }
+      it_behaves_like "an identity matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+
+    context "gimme DSL" do
+      Given(:matcher) { is_a(Shoopuf) }
+      it_behaves_like "an identity matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+  end
+
+  describe Any do
+    context "class API" do
+      Given(:matcher) { Any.new(Shoopuf) }
+      it_behaves_like "an identity matcher"
+      it_behaves_like "a relaxed matcher"
+    end
+
+    context "gimme DSL" do
+      Given(:matcher) { any(Shoopuf) }
+      it_behaves_like "an identity matcher"
+      it_behaves_like "a relaxed matcher"
+    end
+  end
+
+  describe Gimme::Matchers::Numeric do
+    shared_examples_for "a numeric matcher" do
+      Then { matcher.should match 5 }
+      Then { matcher.should match 5.5 }
+    end
+
+    context "class API" do
+      Given(:matcher) { Gimme::Matchers::Numeric.new }
+      it_behaves_like "a numeric matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+
+    context "gimme DSL" do
+      Given(:matcher) { numeric }
+      it_behaves_like "a numeric matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+  end
+
+  describe Gimme::Matchers::Boolean do
+    shared_examples_for "a boolean matcher" do
+      Then { matcher.should match true }
+      Then { matcher.should match false }
+      Then { matcher.should_not match Boolean }
+    end
+
+    context "class API" do
+      Given(:matcher) { Gimme::Matchers::Boolean.new }
+      it_behaves_like "a boolean matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+
+    context "gimme DSL" do
+      Given(:matcher) { boolean }
+      it_behaves_like "a boolean matcher"
+      it_behaves_like "a restrictive matcher"
+    end
+  end
 
 end
