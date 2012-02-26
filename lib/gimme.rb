93c3781b6dc6d697571b48576a9065c626e91fcb
Justin Searls <searls@gmail.com>
2012-02-26 10:42:49 -0500

    fixed rename fail 2

--- lib/gimme.rb
+++ lib/gimme.rb
@@ -1,10 +1,12 @@
 require 'gimme/test_double'
-require 'gimme/method_resolver'
+require 'gimme/resolves_methods'
 require 'gimme/errors'
 require 'gimme/gives'
 require 'gimme/verifies'
 require 'gimme/matchers'
 require 'gimme/captor'
+require 'gimme/invokes_satisfied_stubbing'
+
 require 'gimme/rspec_adapter'
 
 include Gimme
