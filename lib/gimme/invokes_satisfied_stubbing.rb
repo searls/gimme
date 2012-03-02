110799107366ab314f1eaf5854fb18cc1e9ea9ec
Justin Searls <searls@gmail.com>
2012-03-02 16:22:57 -0500

    refactor

--- lib/gimme/invokes_satisfied_stubbing.rb
+++ lib/gimme/invokes_satisfied_stubbing.rb
@@ -5,27 +5,33 @@ module Gimme
     end
 
     def invoke(method, args)
-      matching_stub_block = nil
-      if @stubbings[method]
-        @stubbings[method].each do |stub_args,stub_block|
-          matching = args.size == stub_args.size
-          args.each_index do |i|
-            unless args[i] == stub_args[i] || (stub_args[i].respond_to?(:matches?) && stub_args[i].matches?(args[i]))
-              matching = false
-              break
-            end
-          end
-          matching_stub_block = stub_block if matching
-        end
-      end
-
-      if matching_stub_block
-        matching_stub_block.call
+      if @stubbings[method] && blk = find_matching_stubbing(method, args)
+        blk.call
       elsif method.to_s[-1,1] == '?'
         false
       else
         nil
       end
     end
+
+    private
+
+    def find_matching_stubbing(method, args)
+      match = nil
+
+      @stubbings[method].each do |stub_args,stub_block|
+        matching = args.size == stub_args.size
+        args.each_index do |i|
+          unless args[i] == stub_args[i] || (stub_args[i].respond_to?(:matches?) && stub_args[i].matches?(args[i]))
+            matching = false
+            break
+          end
+        end
+        match = stub_block if matching
+      end
+
+      match
+    end
+
   end
 end
