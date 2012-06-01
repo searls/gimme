81005b9a3228b04c6beb6c24063fba00c92397a6
Justin Searls <searls@gmail.com>
2012-06-01 16:59:06 -0400

    whitespace

--- lib/gimme/verifies.rb
+++ lib/gimme/verifies.rb
@@ -18,16 +18,16 @@ module Gimme
       #gosh, this loop sure looks familiar. just like another ugly loop I know. TODO.
       invoked = 0
       if Gimme.invocations.get(@double, sym)
-         Gimme.invocations.get(@double, sym).each do |invoke_args,count|
-           matching = args.size == invoke_args.size
-           invoke_args.each_index do |i|
-             unless invoke_args[i] == args[i]  || (args[i].respond_to?(:matches?) && args[i].matches?(invoke_args[i]))
-               matching = false
-               break
-             end
-           end
-           invoked += count if matching
-         end
+        Gimme.invocations.get(@double, sym).each do |invoke_args,count|
+          matching = args.size == invoke_args.size
+          invoke_args.each_index do |i|
+            unless invoke_args[i] == args[i]  || (args[i].respond_to?(:matches?) && args[i].matches?(invoke_args[i]))
+              matching = false
+              break
+            end
+          end
+          invoked += count if matching
+        end
       end
 
       if invoked != @times
