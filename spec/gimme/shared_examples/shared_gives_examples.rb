b2209d5700fc76f06cc5fa3818b8704febba31ac
Jason Karns <jason@karns.name>
2013-03-06 16:31:17 -0500

    Add 'after reset' specs to GivesClassMethods

--- spec/gimme/shared_examples/shared_gives_examples.rb
+++ spec/gimme/shared_examples/shared_gives_examples.rb
@@ -4,6 +4,11 @@ module Gimme
       context "no args" do
         When { gives.nibble() { "nom" } }
         Then { subject.nibble.should == "nom" }
+
+        context "after reset" do
+          When { Gimme.reset }
+          Then { subject.nibble.should == nil }
+        end
       end
 
       context "with args" do
@@ -11,12 +16,22 @@ module Gimme
         Then { subject.eat("carrot").should == "crunch" }
         Then { subject.eat("apple").should == nil }
           # Then { lambda{ subject.eat }.should raise_error ArgumentError } # <PENDING
+
+        context "after reset" do
+          When { Gimme.reset }
+          Then { subject.eat("carrot").should == nil }
+        end
       end
 
       context "with arg matchers" do
         When { gives.eat(is_a(String)) { "yum" } }
         Then { subject.eat("fooberry").should == "yum" }
         Then { subject.eat(15).should == nil }
+
+        context "after reset" do
+          When { Gimme.reset }
+          Then { subject.eat("fooberry").should == nil }
+        end
       end
     end
 
