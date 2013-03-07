a6a3737c0c2aaf63af64fe1d626e6dae98ff38e1
Jason Karns <jason@karns.name>
2013-03-07 11:27:02 -0500

    If inherited, rebind original_method as usual; otherwise, invoke super

--- lib/gimme/ensures_class_method_restoration.rb
+++ lib/gimme/ensures_class_method_restoration.rb
@@ -7,11 +7,22 @@ module Gimme
     def ensure(method)
       meta_class = (class << @cls; self; end)
       Gimme.on_reset do
-        if real_method = Gimme.class_methods.get(@cls, method)
-          real_method.owner.instance_eval { define_method method, real_method }
+        if original_method = Gimme.class_methods.get(@cls, method)
+          restored_method = restored_method(meta_class, original_method)
+          meta_class.instance_eval { define_method method, restored_method }
         else
           meta_class.send(:remove_method, method)
         end
+      end
+    end
+
+    private
+
+    def restored_method(meta_class, original_method)
+      if meta_class == original_method.owner
+        original_method
+      else
+        lambda { |*args| super *args }
       end
     end
   end
