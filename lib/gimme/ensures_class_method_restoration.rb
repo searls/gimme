b528bb05efee8d3eeeb06b5fc46542a4ce0678c5
Jason Karns <jason@karns.name>
2013-03-07 10:32:46 -0500

    Ensure that class methods are restored on the method's owner

--- lib/gimme/ensures_class_method_restoration.rb
+++ lib/gimme/ensures_class_method_restoration.rb
@@ -8,7 +8,7 @@ module Gimme
       meta_class = (class << @cls; self; end)
       Gimme.on_reset do
         if real_method = Gimme.class_methods.get(@cls, method)
-          meta_class.instance_eval { define_method method, real_method }
+          real_method.owner.instance_eval { define_method method, real_method }
         else
           meta_class.send(:remove_method, method)
         end
